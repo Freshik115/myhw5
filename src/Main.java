b526c403ea2a2abee721f55681c0d405240f64aa
Andrey <155950115+Freshik115@users.noreply.github.com>
2024-02-01 11:00:57 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
-
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double bmi = service.calculate();
+        double bmi = service.calculate(1.78, 98.45);
         System.out.println(bmi);
     }
 }
