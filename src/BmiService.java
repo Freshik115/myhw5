350109f2095c9124c10a0131e9407001595c5139
Andrey <155950115+Freshik115@users.noreply.github.com>
2024-02-01 11:00:37 +0300

    Update BmiService.java

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,9 +1,7 @@
 public class BmiService {
-    public double calculate() {
-        double height = 1.78; //измерение в метрах
-        double weight = 98.45; //измерение в килограммах
-        double bmi;
-        bmi = weight / (height * height);
+    public double calculate(double height, double weight) {
+
+        double bmi = weight / (height * height);
 
 
         return (int) bmi;
